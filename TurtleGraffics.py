cd44a08691b3027f5aa34bdca433e475d5cc1c0b
zcmp <29331756+zcmp@users.noreply.github.com>
2018-06-22 23:47:29 +1000

    turtle

--- TurtleGraffics.py
+++ TurtleGraffics.py
@@ -1,7 +1,8 @@
 # SquareSpiral1.py - draws a square spiral
 import turtle
+from random import *
 t = turtle.Pen()
-for x in range(100):
-	t.forward(x)
+moves = input("How many moves do you want Turtle to make?")
+for x in range(int(moves)):
+	t.forward(randint(10,100))
 	t.left(90)
-	
